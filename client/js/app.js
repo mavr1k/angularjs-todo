46bf19a403a34414b847029a442c5c4193d6886d
Dmitry Mavrique <hard359@gmail.com>
2018-04-30 16:37:49 +0300

    Finish with js part

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,32 @@
 angular.module('todoApp', [])
   .controller('todoAppController', function ($scope) {
+    var self = this;
+    self.todos = [{
+      done: false,
+      description: 'Wash my dog'
+    }];
+    self.toAdd = {
+      done: false,
+      description: ''
+    };
+    self.adding = false;
+    self.toggleCheckbox = function (todo) {
+      todo.done = !todo.done;
+    }
+    self.isEditable = function () {
+      return self.todos[self.todos.length - 1].description ? true : false;
+    }
+    self.addNew = function () {
+      if (self.toAdd.description) {
+        self.todos.push(Object.assign({}, self.toAdd));
+        self.toAdd.description = '';
+        self.edding = false;
+      }
+    }
+    self.deleteChecked = function () {
+      self.todos = self.todos.filter(el => !el.done);
+    }
+    self.selectAll = function () {
+      self.todos.forEach(el => el.done = true);
+    }
   });
